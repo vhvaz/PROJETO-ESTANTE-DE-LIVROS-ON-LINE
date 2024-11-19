b5f9ed2702e4122fbe336a8bff32741e1e257537
Vitor Hugo <90483530+vhvaz@users.noreply.github.com>
2024-11-18 22:46:41 -0300

    Update BookShelf.js

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,1 +1,18 @@
+import React from "react";
+import Book from "./Book";
+
+function BookShelf({ title, books, onShelfChange }) {
+  return (
+    <div className="bookshelf">
+      <h2>{title}</h2>
+      <div className="books-grid">
+        {books.map((book) => (
+          <Book key={book.id} book={book} onShelfChange={onShelfChange} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BookShelf;
 
